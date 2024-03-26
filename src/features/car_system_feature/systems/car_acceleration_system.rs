3affbcfd11ea257110c6eba8049f072c5b8da573
Boris Proshin <kaktusao@mail.ru>
2024-03-26 22:32:30 +0300

    Improve car movement

--- src/features/car_system_feature/systems/car_acceleration_system.rs
+++ src/features/car_system_feature/systems/car_acceleration_system.rs
@@ -1,9 +1,9 @@
 use bevy::prelude::*;
 use crate::features::car_system_feature::components::car_speed_component::CarSpeed;
 
-const MAX_SPEED: f32 = 4.0;
-const ACCELERATION: f32 = 1.0;
-const BRAKES_ACCELERATION: f32 = 2.0;
+const MAX_SPEED: f32 = 3.0;
+const BRAKES_ACCELERATION: f32 = 1.75;
+const DRAG: f32 = 0.5;
 
 pub fn car_acceleration_system(
     time: Res<Time>,
@@ -11,12 +11,16 @@ pub fn car_acceleration_system(
     mut query: Query<&mut CarSpeed>
 ) {
     let speed_up = keys.pressed(KeyCode::KeyW);
+    let breaks = keys.pressed(KeyCode::KeyS);
 
     for mut car_speed in &mut query {
-        if speed_up {
-            car_speed.speed += time.delta_seconds() * ACCELERATION;
-        } else {
+        if breaks {
             car_speed.speed -= time.delta_seconds() * BRAKES_ACCELERATION;
+        } else if speed_up {
+            let acceleration = (car_speed.speed + 1.0).sqrt() - DRAG;
+            car_speed.speed += time.delta_seconds() * acceleration;
+        } else {
+            car_speed.speed -= time.delta_seconds() * DRAG;
         }
 
         car_speed.speed = car_speed.speed.clamp(0.0, MAX_SPEED);
--- src/features/car_system_feature/systems/car_turn_system.rs
+++ src/features/car_system_feature/systems/car_turn_system.rs
@@ -1,18 +1,28 @@
 use bevy::prelude::*;
+use crate::features::car_system_feature::components::car_speed_component::CarSpeed;
 use crate::features::car_system_feature::components::car_turn_component::CarTurn;
 
-const TURN_SENSITIVITY: f32 = 5.0;
+const TURN_SENSITIVITY: f32 = 10.0;
+const TURN_BASE_POWER: f32 = 2.0;
 
 pub fn car_turn_system(
     time: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut CarTurn>
+    mut query: Query<(&mut CarTurn, &CarSpeed)>
 ) {
     let right = keys.pressed(KeyCode::KeyD);
     let left = keys.pressed(KeyCode::KeyA);
+
     let target = if right { 1.0 } else { 0.0 } + if left { -1.0 } else { 0.0 };
 
-    for mut car_turn in &mut query {
-        car_turn.speed = car_turn.speed.lerp(target, TURN_SENSITIVITY * time.delta_seconds());
+    for (mut car_turn, car_speed) in &mut query {
+        let turn_power = if car_speed.speed != 0.0 {
+            TURN_BASE_POWER / car_speed.speed
+        } else {
+            0.0
+        };
+        car_turn.speed = car_turn.speed.lerp(
+            target * turn_power, TURN_SENSITIVITY * time.delta_seconds()
+        );
     }
 }
